ef8b1b2698d43c02b687036475c8e7d7e28e8b7c
Brian <brian.changchien@gmail.com>
2016-05-16 18:27:02 +0800

    first commit

---
+++ client/Android/Studio/FiWoRDC/src/main/java/com/foxconn/fiwordc/application/GlobalApp.java
@@ -0,0 +1,6 @@
+package com.foxconn.fiwordc.application;
+
+
+public class GlobalApp extends com.freerdp.freerdpcore.application.GlobalApp {
+
+}
---
+++ client/Android/Studio/freeRDPCore/src/main/java/com/freerdp/freerdpcore/presentation/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.freerdp.freerdpcore.presentation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnCreateContextMenuListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.freerdp.freerdpcore.R;
+import com.freerdp.freerdpcore.application.GlobalApp;
+import com.freerdp.freerdpcore.application.GlobalSettings;
+import com.freerdp.freerdpcore.domain.BookmarkBase;
+import com.freerdp.freerdpcore.domain.ConnectionReference;
+import com.freerdp.freerdpcore.domain.PlaceholderBookmark;
+import com.freerdp.freerdpcore.domain.QuickConnectBookmark;
+import com.freerdp.freerdpcore.utils.BookmarkArrayAdapter;
+import com.freerdp.freerdpcore.utils.SeparatedListAdapter;
+import com.freerdp.freerdpcore.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
